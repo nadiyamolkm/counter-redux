02fbf52cefdbb2e1ded1d1101c815caa14bcd3e1
nadiyamolkm <nadiyamolkm@gmail.com>
2024-01-08 20:44:52 +0530

    counter application using redux

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className='d-flex align-items-center justify-content-center w-100 flex-column' style={{ height: "100vh" }}>
+      <div className='d-flex align-items-center justify-content-center p-5 rounded flex-column'
+        style={{ backgroundColor: "white" }}
+      >
+        <h2 className='text-primary'>Counter Application</h2>
+        <Counter />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
